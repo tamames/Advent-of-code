475d5ece5a43484edf397f636970539b5adb6345
tamames <arcospgt@gmail.com>
2023-01-21 22:30:16 +0100

    Added day 4

---
+++ 2022/day_4/code.py
@@ -0,0 +1,56 @@
+
+def check_overlap_1(elf_1: list[str], elf_2: list[str]) -> bool:
+
+    elf_1 = list(map(int, elf_1))
+    elf_2 = list(map(int, elf_2))
+
+    if elf_1[0] <= elf_2[0] and elf_1[1] >= elf_2[1]:
+        return True
+    if elf_1[0] >= elf_2[0] and elf_1[1] <= elf_2[1]:
+        return True
+    
+    return False
+
+
+
+    
+
+def challenge1():
+    with open('./2022/day_4/input.txt') as f:
+        lines = f.read().splitlines()
+    
+    overlaps = 0
+    for line in lines:
+        elf_1, elf_2 = line.split(",")
+        if check_overlap_1(elf_1.split("-"), elf_2.split("-")):
+            overlaps += 1
+    
+    print(overlaps)
+
+
+def check_overlap_2(elf_1: list[str], elf_2: list[str]) -> bool:
+
+    elf_1 = list(map(int, elf_1))
+    elf_2 = list(map(int, elf_2))
+
+    if elf_1[0] > elf_2[1]:
+        return False
+
+    return elf_1[1] >= elf_2[0]
+        
+
+def challenge2():
+    with open('./2022/day_4/input.txt') as f:
+        lines = f.read().splitlines()
+
+    overlaps = 0
+    for line in lines:
+        elf_1, elf_2 = line.split(",")
+        if check_overlap_2(elf_1.split("-"), elf_2.split("-")):
+            overlaps += 1
+
+    print(overlaps)
+
+if __name__ == '__main__':
+    challenge1()
+    challenge2()
